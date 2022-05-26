3bd45b019e7d73f8469427741bd05512f86e3910
Kevin Lacker <lacker@gmail.com>
2022-05-26 11:48:28 -0700

    attempt to write hello_thread

---
+++ hello_thread.c
@@ -0,0 +1,19 @@
+#include "hashpipe.h"
+
+static void *run(hashpipe_thread_args_t * args) {
+  printf("hello world!\n");
+}
+
+static hashpipe_thread_desc_t hello_thread = {
+ name: "hello_thread",
+ skey: "HELLO",
+ init: NULL,
+ run:  run,
+ ibuf_desc: {NULL},
+ obuf_desc: {NULL}
+};
+
+static __attribute__((constructor)) void ctor()
+{
+  register_hashpipe_thread(&hello_thread);
+}
